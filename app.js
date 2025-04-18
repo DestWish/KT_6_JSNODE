034e41e99a43b98d1df6e38c0136026a3cc2b440
Oleg_Markov (DxstW1sh) <gkdestroy@yandex.ru>
2025-04-18 15:17:47 +0300

    Поработал с маршрутизацией. Обработал четыре маршрута: home, about, contact и адресса, не соответсвующего ни одному из маршрутов

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.setHeader('UserId', '666666');
-  res.end();
+    res.setHeader('UserId', '666666');
+    res.setHeader('Content-Type', 'text/html; charset=utf-8;');
+
+    if (req.url === '/home' || req.url === '/') {
+        res.write("<h2>home</h2>")
+    }
+    else if (req.url == '/about') {
+        res.write("<h2>About</h2>")
+    }
+    else if (req.url == '/contact') {
+        res.write("<h2>Contacts</h2>")
+    }
+    else {
+        res.write("<h2>Not Found</h2>")
+    }
+    res.end();
 }).listen(3000);
